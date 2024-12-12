d78b7f08343c7370a32ad298bcbbad5407ed0ecd
halladj <halladj00@gmail.com>
2024-12-12 09:14:45 +0100

    session-3

---
+++ session-2/Session2/Program.cs
@@ -0,0 +1,112 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+
+(int sum, int square) sum_and_square(int a){
+    var r1 = a + a;
+    var r2 = a * a;
+    
+    return (r1, r2);
+}
+
+var (sum, sqaure) = sum_and_square(5);
+
+// Console.WriteLine(sum);
+// Console.WriteLine(sqaure);
+
+
+void Greetings(string name){
+    Console.WriteLine("Hello, "+name);
+}
+
+
+// Greetings("roanldo");
+// Greetings("Zizou");
+// Greetings("Marcelo");
+
+
+
+
+
+
+
+// plain-string:
+var name  = " ronaldo and messi";
+
+// Console.WriteLine(name.Length);
+// Console.WriteLine(name.ToLower());
+// Console.WriteLine(name.ToUpper());
+// Console.WriteLine(String.Join(" ", [name, "and zlatan"]));
+// Console.WriteLine(name.Contains("haha"));
+// var strings_list  = name.Split(" ");
+// Console.WriteLine(strings_list[0]);
+// Console.WriteLine(strings_list[1]);
+// Console.WriteLine(strings_list[2]);
+// Console.WriteLine(name.StartsWith(" "));
+
+
+// string q = "ronaldo";
+// string p = "RONALDO";
+
+// Console.WriteLine(p.ToLower() == q.ToLower());
+// Console.WriteLine(String.Equals(p,q, 
+//     StringComparison.OrdinalIgnoreCase));
+
+
+// Arithmetic
+// int a = 5; int b = 15;
+
+// Console.WriteLine(a+b); // addition.
+// Console.WriteLine(a-b); // subtraction.
+// Console.WriteLine(a/b); // division.
+// Console.WriteLine(a%b); // modular division
+
+// Console.WriteLine(b++); // short-hand notation a = a + 1;
+// Console.WriteLine(++b); // short-hand notation a = a + 1;
+// Console.WriteLine(b);   // short-hand notation a = a + 1;
+
+// Console.WriteLine(b--); // short-hand notation a = a - 1;
+// Console.WriteLine(--b); // short-hand notation a = a - 1;
+// Console.WriteLine(b);   // short-hand notation a = a - 1;
+
+
+// comparition operators
+// int a = 5; int b = 15;
+
+
+// bool isEqual = a == b;
+// bool notEqual= a != b;
+
+// bool isGreater= a > b;
+// bool isLess   = a < b;
+
+// bool isGreaterOrEqual = a >= b;
+// bool isLessOrEqual    = a <= b;
+
+
+
+// Logical Operations. 0 -> false, 1 -> true
+// bool a = false | false & true;
+// bool b = false;
+// // logical and -> && , & 
+// Console.WriteLine(a & b);
+
+// // logical or -> ||, |
+// Console.WriteLine(a | b);
+
+// // logical not -> !
+// Console.WriteLine(!!a);
+
+
+
+
+
+ 
+
+
+
+
+
+// !5 = 5 x 4 x 3 x 2 x 1
+// !8 = 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1
+
---
+++ session-3/Session3/Program.cs
@@ -0,0 +1,243 @@
+﻿
+
+// 20 iteration, start from 4.
+// for(var i =4; i<24; i++){
+//     Console.WriteLine(i);
+// }
+
+
+// Console.Write("\n");
+// // first 10 even number.
+// int j =1;
+// int evenCounter = 0;
+// while(true){
+
+//     if (j>9 && j <15){
+//         j++;
+//         continue;
+//     }
+
+//     if(j % 2 == 0){
+//         Console.Write(j+" ");
+//         evenCounter+=1;
+//     }
+
+//     if(evenCounter == 10){
+//         break;
+//     }
+
+
+//     Thread.Sleep(500);
+//     j++;
+// }
+// Console.Write("\n");
+
+
+
+
+
+
+// s = s.Trim();
+// var a = s.Split(" ");
+// Console.WriteLine(a.Length);
+
+
+// string s = "     this is a string    ";
+// int WordCount(string str){
+//     str = str.Trim();
+//     var a = str.Split(" ");
+
+//    return a;   
+// }
+
+// Console.WriteLine(WordCount(s));
+
+
+// int number = 10;
+// int f = 0;
+
+
+// try{
+//     number = number/f;
+//     Console.WriteLine(number);
+// }
+// catch(DivideByZeroException)
+// {
+//     Console.WriteLine("can not divide by zero");
+// }
+// catch(Exception exp)
+// {
+//     Console.WriteLine("Can not do this instruction"+ exp.GetType());
+// }
+
+
+
+
+
+
+
+// while(true)
+// {
+//     try
+//     {
+//         Console.Write("Please enter a number: ");
+//         string n = Console.ReadLine();
+//         int number = int.Parse(n);
+//         Console.WriteLine("this is your number : "+ number);
+//         break;
+//     }
+//     catch(FormatException)
+//     {
+//         Console.ForegroundColor = ConsoleColor.Red;
+//         Console.WriteLine("You entered a string, please enter a number");
+//         Console.ResetColor();
+//     }
+
+// }
+
+
+
+// short b = 10;
+// byte s = (byte)b;
+
+
+// var x = Convert.ToByte(278.4);
+// Console.WriteLine(x);
+
+
+
+// sum method-defintion
+
+// int CalculateSum(int a, int b)
+// {
+//     int result = a+b;
+//     return result;
+// }
+
+// // method invocation:
+
+// try
+// {
+//     Console.Write("Please Enter the first number: ");
+//     int a = int.Parse(Console.ReadLine());
+
+
+//     Console.Write("Please Enter the second number: ");
+//     int b = int.Parse(Console.ReadLine());
+
+//     var x= CalculateSum(a, b);
+//     Console.WriteLine($"{a} + {b} = {x}");
+// }
+// catch(FormatException)
+// {
+//     Console.ForegroundColor = ConsoleColor.Red;
+//     Console.WriteLine("Please enter a valid number !!");
+//     Console.ResetColor();
+// }
+
+using System.Globalization;
+
+string operation;
+int number1 = 0;
+int number2 = 0;
+try{
+    Console.Write("Inter an operation: +, -, *, /, % : ");
+    operation = Console.ReadLine();
+
+    if (operation != "+" && operation != "-" && 
+        operation != "/" && operation != "%" && operation != "*"){
+
+            throw new InvalidOperationException();
+    }
+    Console.Write("Enter the first number: ");
+    number1 = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter the second number: ");
+    number2 = int.Parse(Console.ReadLine());
+}
+catch(FormatException){
+    Console.WriteLine("Ops, Enter a valid Number");
+}
+catch(InvalidOperationException){
+    Console.WriteLine("Invalid Operation");
+}
+
+// int result;
+// if( operation.Equals("+") ){
+//     result = number1 + number2;
+// }else if(operation.Equals("-")){
+//     result = number1 - number2;
+
+// }else if(operation.Equals("*")){
+//     result = number1 * number2;
+
+// }else if(operation.Equals("/")){
+//     result = number1 / number2;
+
+// }else if(operation.Equals("%")){
+//     result = number1 % number2;
+// } else{
+//     Console.WriteLine("Invalis Operation");
+// }
+
+
+
+
+(string, int, int) GetParams(){
+    try{
+        Console.Write("Enter an operation: +, -, *, /, % : ");
+        operation = Console.ReadLine();
+    
+        if (operation != "+" && operation != "-" && 
+            operation != "/" && operation != "%" && operation != "*"){
+        throw new InvalidOperationException();
+    }
+
+        Console.Write("Enter the first number: ");
+        number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        number2 = int.Parse(Console.ReadLine());
+    }
+    catch(FormatException)
+    {
+        Console.WriteLine("Ops, Enter a valid Number");
+    }
+    catch(InvalidOperationException)
+    {
+        Console.WriteLine("Invalid Operation");
+    }
+    return (operation, number1, number2 );
+}
+
+
+
+
+
+
+int calculator() {
+    string operation="";
+    int number1 = 0;
+    int number2 =0;
+
+    (operation, number1, number2) = GetParams();
+
+    int result = 0;
+    if( operation.Equals("+") ){
+        result = number1 + number2;
+    }else if(operation.Equals("-")){
+        result = number1 - number2;
+
+    }else if(operation.Equals("*")){
+        result = number1 * number2;
+
+    }else if(operation.Equals("/")){
+        result = number1 / number2;
+
+    }else if(operation.Equals("%")){
+        result = number1 % number2;
+    } 
+    return result;
+}
+
+Console.WriteLine(calculator());
